b5f55c90f41d8ba5beb15a07c89bf864d832d731
jfonz412 <jfonzone@outlook.com>
2017-06-29 13:39:51 -0400

    add devise gem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,6 +10,9 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
+  # Devise mailer for production (check tutorial if this doesn't work)
+  config.action_mailer.default_url_options = { host: 'https://mysterious-anchorage-62529.herokuapp.com/'}
+
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
