38fdae6755913f3d43dd6894631a8cd078e4f657
jfonz412 <jfonzone@outlook.com>
2017-06-29 18:30:21 -0400

    move login link tests to integration test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -22,23 +22,4 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   	assert_response :success
     assert_select "title", "Contact"
   end
-
-  test "layout links, logged out" do 
-    get root_path
-    assert_template 'static_pages/home'
-      assert_select "a[href=?]", home_path, count: 2
-      assert_select "a[href=?]", about_path
-      assert_select "a[href=?]", contact_path
-      assert_select "a[href=?]", new_user_session_path
-      assert_select "a[href=?]", new_user_registration_path
-  end
-=begin
-  test "layout when logged in" do
-      sign_in @user
-      assert_select "a[href=?]", home_path, count: 2
-      assert_select "a[href=?]", about_path
-      assert_select "a[href=?]", contact_path
-      assert_select "a[href=?]", destroy_user_session_path
-  end
-=end
 end
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -13,4 +13,14 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", contact_path
     assert_select "a[href=?]", destroy_user_session_path
   end
+
+  test "layout links, logged out" do 
+    get root_path
+    assert_template 'static_pages/home'
+      assert_select "a[href=?]", home_path, count: 2
+      assert_select "a[href=?]", about_path
+      assert_select "a[href=?]", contact_path
+      assert_select "a[href=?]", new_user_session_path
+      assert_select "a[href=?]", new_user_registration_path
+  end
 end
