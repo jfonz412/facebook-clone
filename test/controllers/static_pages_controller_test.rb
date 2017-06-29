01d808a6459ca0855902ce22aa39e8026e2ad260
jfonz412 <jfonzone@outlook.com>
2017-06-29 11:57:18 -0400

    basic header

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,15 +4,18 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "get root url" do
   	get root_url
   	assert_response :success
+    assert_select "title", "Home"
   end
 
   test "get about page" do
   	get about_path
   	assert_response :success
+    assert_select "title", "About"
   end
 
   test "get contact" do
   	get contact_path
   	assert_response :success
+    assert_select "title", "Contact"
   end
 end
