7c11e94b9b3946cc8cc5f584001528b714330ec8
jfonz412 <jfonzone@outlook.com>
2017-07-02 17:46:57 -0400

    inverse friendships

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :friendships, class_name: "Friendship"
-  has_many :friends, through: :friendships, foreign_key: "user_id"
+  has_many :friendships
+  has_many :friends, :through => :friendships
+  # Reverse friendships so we can get at the other side of a relationship
+  has_many :inverse_friendships, class_name: "Friendship", foreign_key: "friend_id"
+  has_many :inverse_friends, :through => :inverse_friendships, source: :user
 end
