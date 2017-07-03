5b94129cae30d611c4d99dddc3fe06e4f7f94f42
jfonz412 <jfonzone@outlook.com>
2017-07-03 16:11:23 -0400

    add friends test

--- test/controllers/friendships_controller_test.rb
+++ test/controllers/friendships_controller_test.rb
@@ -1,5 +1,19 @@
 require 'test_helper'
 
 class FriendshipsControllerTest < ActionDispatch::IntegrationTest
-	
+	include Devise::Test::IntegrationHelpers
+	def setup
+		@user = users(:bob)
+		@other_user = users(:mike)
+	end
+
+	test "must be logged in" do
+		assert_no_difference "Friendship.count" do
+			post friendship_path, params: { :friend_id => @other_user.id }
+		end
+		sign_in @user
+		assert_difference "Friendship.count", 1 do
+			post friendship_path, params: { :friend_id => @other_user.id }
+		end
+	end
 end
