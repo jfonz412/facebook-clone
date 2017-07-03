e2e638047ef26056e180621a7995e1503b6d771c
jfonz412 <jfonzone@outlook.com>
2017-07-02 23:26:16 -0400

    users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
-  get 'friendships/create'
+  get 'users/index'
 
-  get 'friendships/destroy'
+  get 'users/show'
+
+	root 'static_pages#home'
 
   devise_for :users
-  root 'static_pages#home'
+
+  get 'friendships/create'
+  get 'friendships/destroy'
+  get  '/users',    to:   'users#index'
   get  '/home',    to:   'static_pages#home'
   get  '/about',   to:   'static_pages#about'
   get  '/contact', to:   'static_pages#contact'
