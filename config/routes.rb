9417370d239fb10cea84f0397c19b260ce0478de
jfonz412 <jfonzone@outlook.com>
2017-07-02 23:43:30 -0400

    user index

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,13 @@
 Rails.application.routes.draw do
-  get 'users/index'
-
-  get 'users/show'
 
 	root 'static_pages#home'
 
   devise_for :users
-
-  get 'friendships/create'
-  get 'friendships/destroy'
-  get  '/users',    to:   'users#index'
-  get  '/home',    to:   'static_pages#home'
-  get  '/about',   to:   'static_pages#about'
-  get  '/contact', to:   'static_pages#contact'
+  get    '/users',      to:   'users#index'
+  #resources  :users, :only[:show, :index] # might need an actual user model for this..?
+  post   '/friendship', to:   'friendships#create'
+  delete '/friendship', to:   'friendships#destroy'
+  get    '/home',       to:   'static_pages#home'
+  get    '/about',      to:   'static_pages#about'
+  get    '/contact',    to:   'static_pages#contact'
 end
