986c95262cb74d805aea2b9c1790fd7aa0017d13
jfonz412 <jfonzone@outlook.com>
2017-07-03 11:39:12 -0400

    add friends to profile

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,5 +1,11 @@
 class FriendshipsController < ApplicationController
   def create
+  	@friendship = current_user.friendships.build(friend_id: params[:friend_id])
+  	if @friendship.save
+  		redirect_to root_url
+  	else
+  		render users_path
+  	end
   end
 
   def destroy
