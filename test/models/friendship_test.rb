e659b5cfae67b0c611437ef7e39f98145fc43d64
jfonz412 <jfonzone@outlook.com>
2017-07-03 15:54:08 -0400

    friendship model test

--- test/models/friendship_test.rb
+++ test/models/friendship_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class FriendshipTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	def setup
+		@user       = users(:bob)
+		@other_user = users(:mike)
+		@friendship = Friendship.new(user_id: @user.id, friend_id: @other_user.id)
+	end
+	test "should be valid" do
+		assert @friendship.valid?
+	end
+
+	test "must have user_id" do
+		@friendship.user_id = nil
+		assert_not @friendship.valid?
+	end
+
+	test "must have friend_id" do
+		@friendship.friend_id = nil
+		assert_not @friendship.valid?
+	end
 end
