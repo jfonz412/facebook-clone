307eb92572efd8618801af2a65ab1b4c5a838875
jfonz412 <jfonzone@outlook.com>
2017-07-06 18:57:13 -0400

    basic notifications

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+	def notifications
+		if current_user.inverse_friendships.pending.count == 0
+			number_of_notices = nil
+		else
+			number_of_notices = "(#{current_user.inverse_friendships.pending.count})"
+		end
+	end
 end
