7855acd1cf33d2b5d777315424dbe03a3ef4b1a6
jfonz412 <jfonzone@outlook.com>
2017-07-07 12:23:55 -0400

    name field for devise signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  # https://stackoverflow.com/questions/16471498/adding-extra-registration-fields-with-devise
+  # if using devise_controller, do this before anything...
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  # whitelists custom parameters for devise
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
