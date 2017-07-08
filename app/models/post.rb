c9afcaaa5c9315a459795636f91bc8d653187dbd
jfonz412 <jfonzone@outlook.com>
2017-07-08 11:59:11 -0400

    add basic posts to profile

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,12 @@
 class Post < ApplicationRecord
+  default_scope -> { order(created_at: :desc) }
+
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 8000 }
+  
   belongs_to :user
+
+  def formatted_date
+		date.strftime("%m/%d/%Y %H:%M%P")
+	end
 end
