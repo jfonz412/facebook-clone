8beed4e03b5528422f769028a3abe4fc6ca6c8da
jfonz412 <jfonzone@outlook.com>
2017-07-11 16:42:30 -0400

    comment controller and form

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,7 @@ class StaticPagesController < ApplicationController
 	def home 
 		if user_signed_in?
 			@post = Post.new
+			@comment = Comment.new
 			@user_feed = current_user.feed
 		end
 	end
--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,6 @@ Rails.application.routes.draw do
   get    '/contact',    to:   'static_pages#contact'
   post   '/like',       to:   'likes#create'  
   delete '/unlike',     to:   'likes#destroy'
+  post   '/comments',    to:   'comments#create'
+  delete '/comments',    to:   'comments#destroy'
 end
