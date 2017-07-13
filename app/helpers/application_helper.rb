bc9e597bf4215f94e179bf02d6e010dd608d66a2
jfonz412 <jfonzone@outlook.com>
2017-07-13 17:04:27 -0400

    notification badge

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,12 @@ module ApplicationHelper
 		if current_user.inverse_friendships.pending.count == 0
 			number_of_notices = nil
 		else
-			number_of_notices = "(#{current_user.inverse_friendships.pending.count})"
+			number_of_notices = "#{current_user.inverse_friendships.pending.count}"
 		end
+	end
+
+	def profile_and_notices
+    badge = content_tag :span, notifications, class: 'badge'
+    text = raw "Profile #{badge}"
 	end
 end
