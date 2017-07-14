112535d386f350560c26e37bca3161baf735f080
jfonz412 <jfonzone@outlook.com>
2017-07-14 16:44:57 -0400

    delete notifications

--- app/controllers/notices_controller.rb
+++ app/controllers/notices_controller.rb
@@ -1,4 +1,8 @@
 class NoticesController < ApplicationController
+  before_action :authenticate_user!
+  
   def destroy
+  	current_user.notices.delete_all
+  	redirect_back(fallback_location: root_url)
   end
 end
--- test/controllers/notices_controller_test.rb
+++ test/controllers/notices_controller_test.rb
@@ -1,9 +1,28 @@
 require 'test_helper'
 
 class NoticesControllerTest < ActionDispatch::IntegrationTest
-  test "should get destroy" do
-    get notices_destroy_url
-    assert_response :success
+	include Devise::Test::IntegrationHelpers
+
+	def setup
+		@bob = users(:bob)
+		@mike = users(:mike)
+		@notice = @mike.notices.build(other_user: @bob, 
+																	notice_type: "Friendship", 
+																	type_id: 7)
+	end
+
+  test "must be logged in to delete notices" do
+  	@notice.save
+  	assert_no_difference "Notice.count" do
+  		get notices_destroy_path
+  	end
   end
 
+  test "delete notices when logged in" do
+  	@notice.save
+  	sign_in @mike
+  	assert_difference "Notice.count", -1 do
+  		get notices_destroy_path
+  	end
+  end
 end
