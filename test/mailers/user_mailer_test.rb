8932b737be978a6584fa0eb315534313d465cf5d
jfonz412 <jfonzone@outlook.com>
2017-07-17 15:42:40 -0400

    mailer test

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class UserMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	test "Welcome email" do
+		user = users(:bob)
+		mail = UserMailer.welcome_email(user)
+	  assert_equal ["noreply@example.com"], mail.from
+	  assert_match user.name,               mail.body.encoded
+	end
 end
