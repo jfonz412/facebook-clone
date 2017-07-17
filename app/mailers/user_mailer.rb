ac5353a34aa2e21abdc68e58653875676b942c44
jfonz412 <jfonzone@outlook.com>
2017-07-17 12:29:36 -0400

    set up welcome email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,8 @@
 class UserMailer < ApplicationMailer
+	default from: 'noreply@example.com'
+	
+	def welcome_email(user)
+		@user = user
+		mail(to: @user.email, subject: "Thanks for signing up!")
+	end
 end
